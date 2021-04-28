e565f75f5f3ec7b70cf0581fd2eb4b6040861966
rudra pramanik <rudrahimangshoo2020.com>
2021-04-29 03:23:19 +0600

    initial

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3681e167a696ff9f28d58975606f3dff'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1d04642f-8117-490e-a154-fe5f9f41998b'
+};
